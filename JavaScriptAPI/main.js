6cb9a64a3777817a2474849e6ada4fb83b88a133
Stuart-Yee <ki6fzb@gmail.com>
2023-02-25 17:12:48 -0800

    executed in JavaScript

---
+++ JavaScriptAPI/main.js
@@ -0,0 +1,17 @@
+const { default: axios } = require('axios');
+
+require('dotenv').config()
+require('axios');
+const key = process.env.usda_key;
+console.log(key);
+
+const params = {"api_key": key, "pageSize": 25}
+const url = "https://api.nal.usda.gov/fdc/v1/foods/list"
+
+let data = null;
+
+axios.get(url, {params: params})
+    .then(res=>data=res)
+    .then(()=>console.log(data))
+    .catch(err=>console.log(err));
+
